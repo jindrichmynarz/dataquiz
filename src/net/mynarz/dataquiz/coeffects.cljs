97c6aeefeb2f177442a560dd070ddf0d7eaa9f14
Jindrich Mynarz <mynarzjindrich@gmail.com>
2024-07-06 09:46:54 +0200

    Coeffect for local storage

--- src/net/mynarz/dataquiz/coeffects.cljs
+++ src/net/mynarz/dataquiz/coeffects.cljs
@@ -2,6 +2,13 @@
   (:require [re-frame.core :as rf]))
 
 (rf/reg-cofx
+   ::local-store
+   (fn [cofx local-store-key]
+      (assoc-in cofx
+                [::local-store local-store-key]
+                (js->clj (.getItem js/localStorage local-store-key)))))
+
+(rf/reg-cofx
   ::timeout
   (fn [cofx {:keys [id event ms]}]
     (assoc-in cofx
